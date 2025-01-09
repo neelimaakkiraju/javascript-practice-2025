89c2ba075140c3bc6aceeb29920dd8b3f546e400
Neelima Akkiraju <neelimaakkiraju2001@gmail.com>
2025-01-09 10:53:55 +0530

    new changes

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,7 +1,9 @@
-const postData = async ()=>{
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
-    const posts = await response.json()
-    console.log(posts)
-}
 
-postData()
+const objData = async ()=>{
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+    const resultData = await response.json()
+    console.log(resultData)
+}
+objData()
+
+
