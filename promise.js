264547a65053751adfca37241776984234743d6b
Neelima Akkiraju <neelimaakkiraju2001@gmail.com>
2025-02-01 17:10:05 +0530

    completed promise objects

--- promise.js
+++ promise.js
@@ -1,13 +1,31 @@
-const promise = new Promise((resolve,reject)=>{
-   const isResolve = false
-    if(isResolve){
-         console.log(resolve("promise resolved"))
-    }else{
-        console.log(reject("error"))
-    }
-})
-promise.then(result =>
-    console.log(result)
-).catch(()=>{
-    console.log('error!')
-})
+// const promise = new Promise((resolve,reject)=>{
+//    const isResolve = false
+//     if(isResolve){
+//          console.log(resolve("promise resolved"))
+//     }else{
+//         console.log(reject("error"))
+//     }
+// })
+// promise.then(result =>
+//     console.log(result)
+// ).catch(()=>{
+//     console.log('error!')
+// })
+
+
+const  login = () => {
+    return(
+        new Promise((resolve,reject)=>{
+            setTimeout(()=>{
+                console.log("please login")
+                resolve("login successful")
+            })
+
+        })
+    )
+}
+
+
+
+
+login().then((data)=>{console.log(data)})
