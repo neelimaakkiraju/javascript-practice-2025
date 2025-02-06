96701b752a9aff3ab6e6f217b3d41e7da11c575b
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-02-06 15:19:16 +0530

    new changes

--- closures.js
+++ closures.js
@@ -8,3 +8,15 @@ function fun1(){
 }
 const result = fun1()
 result()
+
+
+// function makeFunc() {
+//    const name = "Mozilla";
+//    function displayName() {
+//      console.log(name);
+//    }
+//    return displayName;
+//  }
+ 
+//  const myFunc = makeFunc();
+//  myFunc();
