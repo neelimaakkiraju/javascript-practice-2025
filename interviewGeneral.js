35853540c5614e3668d1e892ea526549aaad7baa
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-03-10 17:46:19 +0530

    new changes

---
+++ interviewGeneral.js
@@ -0,0 +1,15 @@
+console.log("5"-3)   //2        
+console.log("5"+3)    //53
+console.log("5"*3)     //15
+console.log("5"/3)  //1.66
+console.log("5"%3)   //2
+
+
+console.log(2>3>4)  //False
+console.log(2<3<4)  //true
+
+
+let a;
+var b;
+console.log(a)
+console.log(b)
