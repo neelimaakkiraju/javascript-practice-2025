890912b989d9ff4449f863ba7c8613c467169c8f
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-03-20 18:01:51 +0530

    New changes

--- findLeapyear.js
+++ findLeapyear.js
@@ -2,4 +2,13 @@ console.log(false==![]) //true
 
 let abc = "Neelima"
 abc[2] = "w"
-console.log(abc)
+console.log(abc) // Neelima
+
+const Arr = [1,2,3,4]
+
+const [y] = Arr
+
+console.log(y) //returns first element "1"
+
+const result = Arr.splice()
+console.log(result)   //blank array
