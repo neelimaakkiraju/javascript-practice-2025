2d99cc3be9d302a37178c2c0a92732be86659a10
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-04-08 16:52:31 +0530

    array compare

---
+++ compareArr.js
@@ -0,0 +1,13 @@
+let arr1 = [1,2,3]
+
+let arr2 = [3,4,5]
+
+let result = arr1 == arr2
+
+console.log(result)
+
+
+// let result2 = arr2 == arr1
+
+// console.log(result2)
+console.log(JSON.stringify(arr1) === JSON.stringify(arr2));
