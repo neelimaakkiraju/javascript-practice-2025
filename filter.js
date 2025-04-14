37e7da92c501c51cf7d16e4dbf54b06bb9eccd86
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-04-14 17:40:30 +0530

    new changes

--- filter.js
+++ filter.js
@@ -3,3 +3,12 @@ const Arr = [1,2,3,4,5,6]
 const result = Arr.filter(num=>num=3)
 
 console.log(result)
+
+
+const result2 = Arr.map(num=>num=3)
+
+console.log(result)
+
+const result3 = Arr.reduce(num=>num=3)
+
+console.log(result)
