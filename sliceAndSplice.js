6fba25120df989facd0d586ee12d8642e325d9f4
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-04-15 16:36:18 +0530

    new changes

--- sliceAndSplice.js
+++ sliceAndSplice.js
@@ -2,8 +2,11 @@ const arr = [1,2,3,4,5]
 
 const result1 = arr.slice(0,2)
 
-const result2 = arr.splice(0,3,"9")
+const result2 = arr.splice(0,3,4)
 
 console.log(result1)
 
 console.log(result2)
+
+
+console.log(0/0)
